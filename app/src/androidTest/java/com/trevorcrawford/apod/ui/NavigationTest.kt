3d7083e0a067e8a9403eb05479b268a956191450
tcrawford94 <17533959+tcrawford94@users.noreply.github.com>
2023-01-12 16:47:52 -0600

    NavigationTest: Add Coverage for Activity level SnackbarManager
    - Ensure snackbarManager.showMessage() works.

--- app/src/androidTest/java/com/trevorcrawford/apod/ui/NavigationTest.kt
+++ app/src/androidTest/java/com/trevorcrawford/apod/ui/NavigationTest.kt
@@ -2,11 +2,15 @@ package com.trevorcrawford.apod.ui
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import com.trevorcrawford.apod.R
 import com.trevorcrawford.apod.data.di.fakeAstronomyPictures
+import com.trevorcrawford.apod.ui.util.SnackbarManager
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
 @HiltAndroidTest
 class NavigationTest {
@@ -17,10 +21,27 @@ class NavigationTest {
     @get:Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+    @Inject
+    lateinit var snackbarManager: SnackbarManager
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
     @Test
-    fun test1() {
-        // TODO: Add navigation tests
+    fun main_activity_launches_to_astronomy_pictures_screen() {
+        // TODO: Add more navigation tests
         composeTestRule.onNodeWithText(fakeAstronomyPictures.first().title, substring = true).assertExists()
+    }
+
+    @Test
+    fun snackbarManager_show_message_displays_snackbar() {
+        // when
+        snackbarManager.showMessage(R.string.error_unable_to_load_pictures)
+
+        // then
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.error_unable_to_load_pictures)).assertExists()
     }
 }
 
