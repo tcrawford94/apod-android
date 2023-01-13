b74c9fde7d57d3135aafaa87ccb63267caa5c270
tcrawford94 <17533959+tcrawford94@users.noreply.github.com>
2023-01-13 12:34:16 -0600

    Add Automatic Version Catalog Update & Update Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,7 @@
 // Root build.gradle.kts
+plugins {
+    @Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
+    alias(libs.plugins.benmanes.versions)
+    @Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
+    alias(libs.plugins.littlerobots.versioncatalogupdate)
+}
